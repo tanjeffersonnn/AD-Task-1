f90334d08342d1a8e3f96c378c24e101dbaa0b97
tanjeffersonnn <tanjeffersonemmanuel@gmail.com>
2025-05-20 08:10:20 +0800

    Delete page2_summary.py

--- page2_summary.py
+++
@@ -1,6 +0,0 @@
-remarks = ["Good", "Average", "Excellent", "Satisfactory", "Excellent"]
-subjects = ["Math", "English", "Science", "History", "PE"]
-
-print("\n=== Subject Remarks ===")
-for i in range(len(subjects)):
-    print(f"{subjects[i]}: {remarks[i]}")
