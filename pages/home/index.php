b0f21f635427cf9ef4a14407bb1379ba9d8c3c04
tanjeffersonnn <tanjeffersonemmanuel@gmail.com>
2025-07-16 19:49:15 +0800

    Delete pages directory

--- pages/home/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$pageTitle = 'Neo\'s Calendar - My Tasks';
-
-require_once APP_ROOT . '/staticData/todos.staticdata.php';
-
-$tasks = $todosArray;
-
-require_once APP_ROOT . '/components/templates/todo-list.component.php';
